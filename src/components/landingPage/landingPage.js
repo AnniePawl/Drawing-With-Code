b4566a702ce268504310d95e131f383ad1ecea46
Anna Pawl <annampawl@gmail.com>
2020-06-29 14:07:50 -0400

    setting up landing page and navbar components

---
+++ src/components/landingPage/landingPage.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './styles/landingPage.scss'
+import Navbar from '../navbar/navbar';
+
+function LandingPage() {
+	return (
+		<div className = 'landing_page_container'>
+			<Navbar />
+		</div>
+	);
+}
+
+export default LandingPage;
---
+++ src/components/navbar/navbar.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import './styles/navbar.scss'
+
+function Navbar() {
+	return (
+		<div className="navbar">
+			<ul>
+				<li>Home</li>
+				<li>Resources</li>
+			</ul>
+		</div>
+	);
+}
+
+export default Navbar;
