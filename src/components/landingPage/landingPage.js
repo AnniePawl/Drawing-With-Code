65e83d1f3d6d9034cb9abde43087c9d4b88359cd
Anna Pawl <annampawl@gmail.com>
2020-06-29 14:35:03 -0400

    resources component in progress

--- src/components/landingPage/landingPage.js
+++ src/components/landingPage/landingPage.js
@@ -1,11 +1,19 @@
 import React from 'react';
 import './styles/landingPage.scss'
 import Navbar from '../navbar/navbar';
+import Resources from '../resources/resources'
 
 function LandingPage() {
 	return (
 		<div className = 'landing_page_container'>
 			<Navbar />
+
+      {/* Resources Container */}
+      <div id="resources" className='resources__section'>
+        <Resources/>
+      </div>
+
+
 		</div>
 	);
 }
---
+++ src/components/resources/resources.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import './styles/resources.scss'
+
+
+function Resources() {
+  return(
+    <div className='resources__container'>
+      <div className= 'resources__content'>
+      <h1 className = 'resources__heading'>Resources</h1>
+      <ul>
+        <li>CodePen</li>
+        <li>Drawing with Code Medium Article </li>
+      </ul>
+      </div>
+    </div>
+  )
+}
+
+export default Resources
