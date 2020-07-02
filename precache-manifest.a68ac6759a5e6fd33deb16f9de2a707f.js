2f532c0b7f749f1cf41377b104ac12296d2da69d
Anna Pawl <annampawl@gmail.com>
2020-07-02 11:58:47 -0400

    Updates

---
+++ precache-manifest.a68ac6759a5e6fd33deb16f9de2a707f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4bfbe10f532be6904ca3aa0f807f9de7",
+    "url": "/Drawing-With-Code/index.html"
+  },
+  {
+    "revision": "d0cec3b3fb2bc1bd4f44",
+    "url": "/Drawing-With-Code/static/css/main.b0241ef5.chunk.css"
+  },
+  {
+    "revision": "62481d50a0579e852b45",
+    "url": "/Drawing-With-Code/static/js/2.61ee8689.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/Drawing-With-Code/static/js/2.61ee8689.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d0cec3b3fb2bc1bd4f44",
+    "url": "/Drawing-With-Code/static/js/main.00b865af.chunk.js"
+  },
+  {
+    "revision": "66d8b930804dd6c9493f",
+    "url": "/Drawing-With-Code/static/js/runtime-main.13ffb5a1.js"
+  }
+]);
---
+++ service-worker.js
@@ -0,0 +1,39 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
+
+importScripts(
+  "/Drawing-With-Code/precache-manifest.a68ac6759a5e6fd33deb16f9de2a707f.js"
+);
+
+self.addEventListener('message', (event) => {
+  if (event.data && event.data.type === 'SKIP_WAITING') {
+    self.skipWaiting();
+  }
+});
+
+workbox.core.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/Drawing-With-Code/index.html"), {
+  
+  blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
+});
