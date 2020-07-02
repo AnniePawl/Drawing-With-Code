aae051abf378466bb89e778ffbad9018615ef4be
Anna Pawl <annampawl@gmail.com>
2020-07-02 11:08:31 -0400

    added pom component, styled home

--- src/components/home/home.js
+++ src/components/home/home.js
@@ -1,11 +1,19 @@
 import React from 'react';
+import Pom from './pom'
 import './styles/home.scss';
 
 function Home() {
 	return (
 		<div className="home__container">
-			<h1>CSS ART</h1>
-			<h2>A Guide to Drawing with Code</h2>
+			{/* Home Heading */}
+			<div className='home__heading'>
+				<h1>CSS ART</h1>
+				<h2> A Guide to Drawing with Code</h2>
+			</div>
+			{/* Pom Container */}
+			<div className='pom__container'>
+				<Pom/>
+			</div>
 		</div>
 	);
 }
