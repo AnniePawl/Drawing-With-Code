6f057cef43dffb10a2301515d8780d11f3b70e70
Anna Pawl <annampawl@gmail.com>
2020-07-14 11:11:44 -0400

    updating resources component

--- src/components/resources/resources.js
+++ src/components/resources/resources.js
@@ -6,12 +6,38 @@ function Resources() {
   return(
     <div className='resources__container'>
       <div className= 'resources__content'>
-      <h1 className >Resources</h1>
-      <ul>
-        <li>CodePen</li>
-        {/* https://codepen.io/ */}
-        <li>Drawing with Code Medium Article </li>
-      </ul>
+        <h1 className >Resources</h1>
+
+        <div className='resources__lists'>
+
+          {/* My Resources */}
+          <div className='resources__my_resources'>
+            <h2 className='resources__subheading'>resources by me</h2>
+            <ul>
+              {/* My medium article */}
+              <li><a href="https://medium.com/@annapawl/drawing-with-code-716cb0b21b06 " target='blank'>Drawing with Code Medium Article </a></li>
+            </ul>
+          </div>
+
+          {/* Other Resources */}
+          <div className='resources__other_resources'>
+            <h2 className='resources__subheading'>other great resources</h2>
+            <ul>
+              {/* CodePen */}
+              <li>
+                <a href="https://codepen.io/" target='blank'>CodePen</a><br/>
+                 <p>An online code editor and community of front-end web developers. This platform allows you to test and showcase code snippets built with HTML, CSS, and JS.</p>
+              </li>
+              {/* Pure CSS Art Gallery */}
+              <li>
+                <a href='https://css-art.com/' target='blank'>Pure CSS Art Gallery</a><br/>
+                <p>Awesome, self-maintained, open source gallery for CSS web art. Great place to get inspired and connect with fellow artists.</p>
+              </li>
+         
+            </ul>
+          </div>
+
+        </div>
       </div>
     </div>
   )
